1fd5d687094170c7fbb11d6c24763d71b1582574
Dirk Stelzner <d.stelzner@dashoefer.de>
2022-10-27 11:55:01 +0200

    Testkommentar hinzugefügt

--- org.eclipse.forkexample.calc/src/org/eclipse/example/calc/internal/Calculator.java
+++ org.eclipse.forkexample.calc/src/org/eclipse/example/calc/internal/Calculator.java
@@ -7,6 +7,9 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *******************************************************************************/
+
+// Hallo!
+
 package org.eclipse.example.calc.internal;
 
 import org.eclipse.example.calc.BinaryOperation;
